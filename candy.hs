a56105a1bc7973ff90d28f8b4554e109b4188c54
john mangual <john.mangual@gmail.com>
2017-04-03 19:06:59 -0400

    nice!

---
+++ candy.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Reflex.Dom
+import Data.Map
+import Data.Text (Text, pack)
+
+-------------------------------------------------------------------------------
+
+ns :: Maybe Text
+ns = Just "http://www.w3.org/2000/svg"
+
+svgAttributes :: Map Text Text
+svgAttributes = ( "style" =: "width:300px; height:150px; background-color:#F0F0F0;" )
+
+squareAttributes :: Map Text Text
+squareAttributes = fromList [("x", "0"), ("y", "0"), ("width", "10"), ("height", "10"), ("fill", "#F0F0F0")]
+
+svgButton :: DomBuilder t m => Maybe ( Map Text Text ) -> m (Event t ())
+svgButton attr = case attr of
+  Just attributes -> do
+    (e, _) <- element "svg" ( def & initialAttributes .~ mapKeys (AttributeName Nothing) attributes & namespace .~ Just "http://www.w3.org/2000/svg" ) $ blank
+    return $ domEvent Click e
+  Nothing         -> do
+    (e, _) <- element "svg" def $ blank
+    return $ domEvent Click e
+
+
+main = mainWidget $ do
+
+  el "div" $ do
+    -- in this monad each computation returns an HTML artifact
+    btn <- svgButton ( Just svgAttributes )
+    c   <- count ( btn )
+    el "div" $ dynText $ fmap ( pack . show ) $ c
+    return ()
+  return ()
