e026cfff77f2fc120ac3c347e7128ff9c5e412c7
john mangual <john.mangual@gmail.com>
2017-04-07 14:33:06 -0400

    the random number generator is not random

--- candy.hs
+++ candy.hs
@@ -3,6 +3,7 @@
 import Reflex.Dom
 import Data.Map
 import Data.Text (Text, pack)
+import System.Random
 
 -------------------------------------------------------------------------------
 
@@ -12,8 +13,11 @@ ns = Just "http://www.w3.org/2000/svg"
 svgAttributes :: Map Text Text
 svgAttributes = ( "style" =: "width:300px; height:150px; background-color:#F0F0F0;" )
 
-squareAttributes :: Map Text Text
-squareAttributes = fromList [("x", "0"), ("y", "0"), ("width", "10"), ("height", "10"), ("fill", "#F0F0F0")]
+colors :: [ Text ]
+colors = fmap pack [ "#DEB225", "#EB5044", "#602FFF", "#D5F531" ]
+
+squareAttributes :: (Int, Int) -> Text -> Map Text Text
+squareAttributes ( a, b ) color = fromList [("x", ( pack . show ) a ), ("y", ( pack . show ) a ), ("width", "10"), ("height", "10"), ("fill", color )]
 
 svgButton :: DomBuilder t m => Maybe ( Map Text Text ) -> m a -> m (Event t ())
 svgButton attr child = case attr of
@@ -24,12 +28,28 @@ svgButton attr child = case attr of
     (e, _) <- element "svg" ( def & namespace .~ Just "http://www.w3.org/2000/svg" ) child
     return $ domEvent Click e
 
+square :: DomBuilder t m => Int -> m a -> m (Event t ())
+square n child =
+  let
+
+    attributes = squareAttributes ( 0 , 0 ) $ colors !! n
+  in
+    do
+    (e, _) <- element "rect" ( def & initialAttributes .~ mapKeys (AttributeName Nothing) attributes & namespace .~ Just "http://www.w3.org/2000/svg" ) child
+    return $ domEvent Click e
+
+t :: RandomGen g => g -> (Int, g)
+t = randomR (0, 3 :: Int) 
+
+n :: Int
+n = fst <$> randomR (0, 3 :: Int) $ mkStdGen 42
+
 
 main = mainWidget $ do
 
   el "div" $ do
     -- in this monad each computation returns an HTML artifact
-    btn <- svgButton ( Just svgAttributes ) $ blank
+    btn <- svgButton ( Just svgAttributes ) $ ( square n  ) blank
     c   <- count ( btn )
     el "div" $ dynText $ fmap ( pack . show ) $ c
     return ()
