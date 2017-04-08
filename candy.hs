2f885a6a80bd811fdd81bd2818b517eebaf1ed8e
john mangual <john.mangual@gmail.com>
2017-04-08 10:34:49 -0400

    solution with recursive do

--- candy.hs
+++ candy.hs
@@ -1,4 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecursiveDo       #-}
+
+-- not sure if I like "rec" or "do"
+-- http://stackoverflow.com/questions/5405850/how-does-the-haskell-rec-keyword-work
+-- http://stackoverflow.com/questions/16726659/should-do-notation-be-avoided-in-haskell
 
 import Reflex.Dom
 import Data.Map
@@ -31,19 +36,22 @@ svgButton attr child = case attr of
 square :: DomBuilder t m => Int -> m a -> m (Event t ())
 square n child =
   let
-
     attributes = squareAttributes ( 0 , 0 ) $ colors !! n
   in
     do
     (e, _) <- element "rect" ( def & initialAttributes .~ mapKeys (AttributeName Nothing) attributes & namespace .~ Just "http://www.w3.org/2000/svg" ) child
     return $ domEvent Click e
 
+
+--piece = do
+--  rec (e, _) <- el' "div" $ display =<< count (domEvent Click e)
+--  return domEvent Click e
+
 t :: RandomGen g => g -> (Int, g)
-t = randomR (0, 3 :: Int) 
+t = randomR (0, 3 :: Int)
 
 n :: Int
 n = fst <$> randomR (0, 3 :: Int) $ mkStdGen 42
-
 
 main = mainWidget $ do
 
@@ -52,5 +60,7 @@ main = mainWidget $ do
     btn <- svgButton ( Just svgAttributes ) $ ( square n  ) blank
     c   <- count ( btn )
     el "div" $ dynText $ fmap ( pack . show ) $ c
+    -- help from IRC
+    rec (e, _) <- elAttr' "div" ( fromList [("style", "background-color:#DEB225; width:12pt;")])  $ display =<< count (domEvent Click e)
     return ()
   return ()
